f24e377573ac85227db632fa68b81a6cdab7b26d
Brayan Styvison <99987766+BStyvison@users.noreply.github.com>
2022-11-04 09:21:24 -0300

    Update Comment.jsx

--- Ignite/React/01-fundamentos-reactjs/src/components/Comment.jsx
+++ Ignite/React/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -5,7 +5,22 @@ export const Comment = () => {
     <div className={style.comment}>
       <img src="https://github.com/BStyvison.png" alt="avatar" />
 
-      <div className={style.commentBox}></div>
+      <div className={style.commentBox}>
+        <div className={style.comentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Brayan Styvison</strong>
+              <time title="23 de outubro ás 09:59" dataTime="2022-10-23 09:57:38"></time>
+            </div>
+          </header>
+          <p>Muito bom dev, parabéns👏👏</p>
+        </div>
+
+        <footer>
+          Aplaudir
+        </footer>
+      </div>
+      
     </div>
   );
 };
